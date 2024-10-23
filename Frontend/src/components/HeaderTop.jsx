ed0ac6ccc4ebcea3c3b52b2f85de2d1a0146e4da
Elmerson Torres <elmerson.t@hotmail.com>
2024-10-23 17:09:14 -0300

    Header top listo

--- Frontend/src/components/HeaderTop.jsx
+++ Frontend/src/components/HeaderTop.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import '../styles/HeaderTop.css';
 
 function HeaderTop() {
   return (
@@ -7,17 +8,17 @@ function HeaderTop() {
       <Container>
         <Row className="justify-content-md-center">
           <Col xs lg="4" className="d-flex align-items-center justify-content-center">
-            <a href="https://www.google.com/maps/place/San+Nicolás+681,+8930049+San+Miguel,+Región+Metropolitana/@-33.501599,-70.6447445,17z/data=!3m1!4b1!4m6!3m5!1s0x9662dab90382d4d5:0xa224325777439b92!8m2!3d-33.501599!4d-70.6421696!16s%2Fg%2F11gr7t9s0g?entry=ttu&g_ep=EgoyMDI0MTAyMC4xIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noopener noreferrer">
+            <a className='enlace' href="https://www.google.com/maps/place/San+Nicolás+681,+8930049+San+Miguel,+Región+Metropolitana/@-33.501599,-70.6447445,17z/data=!3m1!4b1!4m6!3m5!1s0x9662dab90382d4d5:0xa224325777439b92!8m2!3d-33.501599!4d-70.6421696!16s%2Fg%2F11gr7t9s0g?entry=ttu&g_ep=EgoyMDI0MTAyMC4xIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noopener noreferrer">
               <i className="bi bi-geo-alt"></i> San Nicolás 681, San Miguel
             </a>
           </Col>
           <Col xs lg="4" className="d-flex align-items-center justify-content-center">
-            <a href="https://wa.me/56927096973" target="_blank" rel="noopener noreferrer">
+            <a className='enlace' href="https://wa.me/56927096973" target="_blank" rel="noopener noreferrer">
               <i className="bi bi-whatsapp"></i> +56927096973
             </a>
           </Col>
           <Col xs lg="4" className="d-flex align-items-center justify-content-center">
-            <a href="https://www.instagram.com/colegiolucilagodoy/" target="_blank" rel="noopener noreferrer">
+            <a className='enlace' href="https://www.instagram.com/colegiolucilagodoy/" target="_blank" rel="noopener noreferrer">
               <i className="bi bi-instagram"></i> colegiolucilagodoy
             </a>
           </Col>
