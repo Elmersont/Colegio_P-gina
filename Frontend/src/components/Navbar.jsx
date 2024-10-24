4ce21e73e303ae77c09d9c6900a60ab6a4c07929
Elmerson Torres <elmerson.t@hotmail.com>
2024-10-24 06:03:19 -0300

    diseño navbar listo

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -6,22 +6,40 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import '../styles/Navbar.css';
 
 function NavbarColegio() {
+  const [isMobile, setIsMobile] = useState(false);
   const [showAdmisionDropdown, setShowAdmisionDropdown] = useState(false);
   const [showNuestroColegioDropdown, setShowNuestroColegioDropdown] = useState(false);
   const [showDocumentosDropdown, setShowDocumentosDropdown] = useState(false);
   const [showCursosDropdown, setShowCursosDropdown] = useState(false);
 
-  const handleMouseEnterAdmision = () => setShowAdmisionDropdown(true);
-  const handleMouseLeaveAdmision = () => setShowAdmisionDropdown(false);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 1200);
+    };
 
-  const handleMouseEnterNuestroColegio = () => setShowNuestroColegioDropdown(true);
-  const handleMouseLeaveNuestroColegio = () => setShowNuestroColegioDropdown(false);
+    handleResize();
+    window.addEventListener('resize', handleResize);
 
-  const handleMouseEnterDocumentos = () => setShowDocumentosDropdown(true);
-  const handleMouseLeaveDocumentos = () => setShowDocumentosDropdown(false);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
-  const handleMouseEnterCursos = () => setShowCursosDropdown(true);
-  const handleMouseLeaveCursos = () => setShowCursosDropdown(false);
+  const handleMouseEnter = (setDropdown) => {
+    if (!isMobile) {
+      setDropdown(true);
+    }
+  };
+
+  const handleMouseLeave = (setDropdown) => {
+    if (!isMobile) {
+      setDropdown(false);
+    }
+  };
+
+  const handleDropdownClick = (dropdownState, setDropdown) => {
+    if (isMobile) {
+      setDropdown(!dropdownState);
+    }
+  };
 
   return (
     <Navbar expand="xl" className="navbar-custom">
@@ -42,68 +60,66 @@ function NavbarColegio() {
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0 ms-5 d-flex justify-content-between"
-            style={{ width: '90%' }} 
+            style={{ width: '90%' }}
             navbarScroll
           >
             <Nav.Link href="#action1">INICIO</Nav.Link>
 
-            <NavDropdown 
-              title="ADMISIÓN" 
+            <NavDropdown
+              title="ADMISIÓN"
               id="navbarScrollingDropdown"
-              show={showAdmisionDropdown} 
-              onMouseEnter={handleMouseEnterAdmision} 
-              onMouseLeave={handleMouseLeaveAdmision}
+              show={showAdmisionDropdown}
+              onMouseEnter={() => handleMouseEnter(setShowAdmisionDropdown)}
+              onMouseLeave={() => handleMouseLeave(setShowAdmisionDropdown)}
+              onClick={() => handleDropdownClick(showAdmisionDropdown, setShowAdmisionDropdown)}
             >
               <NavDropdown.Item href="#action3">Admisión</NavDropdown.Item>
               <NavDropdown.Item href="#action4">Otra acción</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">Algo más aquí</NavDropdown.Item>
-            </NavDropdown>
-            
-            <NavDropdown 
-              title="NUESTRO COLEGIO" 
-              id="navbarScrollingDropdownLink"
-              show={showNuestroColegioDropdown} 
-              onMouseEnter={handleMouseEnterNuestroColegio} 
-              onMouseLeave={handleMouseLeaveNuestroColegio}
-            >
-              <NavDropdown.Item href="#action3">Acción</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">Otra acción</NavDropdown.Item>
-              <NavDropdown.Divider />
               <NavDropdown.Item href="#action5">Algo más aquí</NavDropdown.Item>
             </NavDropdown>
 
-            <NavDropdown 
-              title="CURSOS" 
-              id="navbarScrollingDropdownCursos" 
-              show={showCursosDropdown} 
-              onMouseEnter={handleMouseEnterCursos} 
-              onMouseLeave={handleMouseLeaveCursos} 
+            <NavDropdown
+              title="NUESTRO COLEGIO"
+              id="navbarScrollingDropdownLink"
+              show={showNuestroColegioDropdown}
+              onMouseEnter={() => handleMouseEnter(setShowNuestroColegioDropdown)}
+              onMouseLeave={() => handleMouseLeave(setShowNuestroColegioDropdown)}
+              onClick={() => handleDropdownClick(showNuestroColegioDropdown, setShowNuestroColegioDropdown)}
             >
-              <NavDropdown.Item href="#action3">Admisión</NavDropdown.Item>
+              <NavDropdown.Item href="#action3">Acción</NavDropdown.Item>
               <NavDropdown.Item href="#action4">Otra acción</NavDropdown.Item>
-              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">Algo más aquí</NavDropdown.Item>
+            </NavDropdown>
+
+            <NavDropdown
+              title="CURSOS"
+              id="navbarScrollingDropdownCursos"
+              show={showCursosDropdown}
+              onMouseEnter={() => handleMouseEnter(setShowCursosDropdown)}
+              onMouseLeave={() => handleMouseLeave(setShowCursosDropdown)}
+              onClick={() => handleDropdownClick(showCursosDropdown, setShowCursosDropdown)}
+            >
+              <NavDropdown.Item href="#action3">Acción</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">Otra acción</NavDropdown.Item>
               <NavDropdown.Item href="#action5">Algo más aquí</NavDropdown.Item>
             </NavDropdown>
 
             <Nav.Link href="#action1">NOTICIAS</Nav.Link>
 
-            <NavDropdown 
-              title="DOCUMENTOS" 
+            <NavDropdown
+              title="DOCUMENTOS"
               id="navbarScrollingDropdownDocumentos"
               show={showDocumentosDropdown}
-              onMouseEnter={handleMouseEnterDocumentos}
-              onMouseLeave={handleMouseLeaveDocumentos}
+              onMouseEnter={() => handleMouseEnter(setShowDocumentosDropdown)}
+              onMouseLeave={() => handleMouseLeave(setShowDocumentosDropdown)}
+              onClick={() => handleDropdownClick(showDocumentosDropdown, setShowDocumentosDropdown)}
             >
-              <NavDropdown.Item href="#action3">Admisión</NavDropdown.Item>
+              <NavDropdown.Item href="#action3">Acción</NavDropdown.Item>
               <NavDropdown.Item href="#action4">Otra acción</NavDropdown.Item>
-              <NavDropdown.Divider />
               <NavDropdown.Item href="#action5">Algo más aquí</NavDropdown.Item>
             </NavDropdown>
 
-            <Nav.Link href="#">
-              CONTACTO
-            </Nav.Link>
+            <Nav.Link href="#">CONTACTO</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
