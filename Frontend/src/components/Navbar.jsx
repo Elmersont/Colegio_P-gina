125a0836c30feace92c4d205e87f8d814c65c64b
Elmerson Torres <elmerson.t@hotmail.com>
2024-10-23 22:12:16 -0300

    mejoras en navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,56 +1,110 @@
-import Button from 'react-bootstrap/Button';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import '../styles/Navbar.css';
 
 function NavbarColegio() {
+  const [showAdmisionDropdown, setShowAdmisionDropdown] = useState(false);
+  const [showNuestroColegioDropdown, setShowNuestroColegioDropdown] = useState(false);
+  const [showDocumentosDropdown, setShowDocumentosDropdown] = useState(false);
+  const [showCursosDropdown, setShowCursosDropdown] = useState(false);
+
+  const handleMouseEnterAdmision = () => setShowAdmisionDropdown(true);
+  const handleMouseLeaveAdmision = () => setShowAdmisionDropdown(false);
+
+  const handleMouseEnterNuestroColegio = () => setShowNuestroColegioDropdown(true);
+  const handleMouseLeaveNuestroColegio = () => setShowNuestroColegioDropdown(false);
+
+  const handleMouseEnterDocumentos = () => setShowDocumentosDropdown(true);
+  const handleMouseLeaveDocumentos = () => setShowDocumentosDropdown(false);
+
+  const handleMouseEnterCursos = () => setShowCursosDropdown(true);
+  const handleMouseLeaveCursos = () => setShowCursosDropdown(false);
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="xl" className="navbar-custom">
       <Container fluid>
         <Navbar.Brand href="#" className="d-flex align-items-center ms-5">
-            <img
-            src="/assets/images/Logo CLG.jpeg" // Ruta de la imagen en la carpeta public
+          <img
+            src="/assets/images/Logo CLG.jpeg"
             alt="Escudo Colegio Lucila Godoy"
-            width="90" // Ancho de la imagen, puedes ajustar según sea necesario
-            height="130" // Alto de la imagen, ajustable
+            width="90"
+            height="130"
             className="d-inline-block align-top"
-            />
-            <span className="ms-3">Colegio Lucila Godoy</span>
+          />
+          <span className="ms-3">Colegio Lucila Godoy</span>
         </Navbar.Brand>
+
         <Navbar.Toggle aria-controls="navbarScroll" />
+
         <Navbar.Collapse id="navbarScroll">
           <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
+            className="me-auto my-2 my-lg-0 ms-5 d-flex justify-content-between"
+            style={{ width: '90%' }} 
             navbarScroll
           >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
+            <Nav.Link href="#action1">INICIO</Nav.Link>
+
+            <NavDropdown 
+              title="ADMISIÓN" 
+              id="navbarScrollingDropdown"
+              show={showAdmisionDropdown} 
+              onMouseEnter={handleMouseEnterAdmision} 
+              onMouseLeave={handleMouseLeaveAdmision}
+            >
+              <NavDropdown.Item href="#action3">Admisión</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">Otra acción</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action5">Algo más aquí</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
+            
+            <NavDropdown 
+              title="NUESTRO COLEGIO" 
+              id="navbarScrollingDropdownLink"
+              show={showNuestroColegioDropdown} 
+              onMouseEnter={handleMouseEnterNuestroColegio} 
+              onMouseLeave={handleMouseLeaveNuestroColegio}
+            >
+              <NavDropdown.Item href="#action3">Acción</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">Otra acción</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">Algo más aquí</NavDropdown.Item>
+            </NavDropdown>
+
+            <NavDropdown 
+              title="CURSOS" 
+              id="navbarScrollingDropdownCursos" 
+              show={showCursosDropdown} 
+              onMouseEnter={handleMouseEnterCursos} 
+              onMouseLeave={handleMouseLeaveCursos} 
+            >
+              <NavDropdown.Item href="#action3">Admisión</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">Otra acción</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">Algo más aquí</NavDropdown.Item>
+            </NavDropdown>
+
+            <Nav.Link href="#action1">NOTICIAS</Nav.Link>
+
+            <NavDropdown 
+              title="DOCUMENTOS" 
+              id="navbarScrollingDropdownDocumentos"
+              show={showDocumentosDropdown}
+              onMouseEnter={handleMouseEnterDocumentos}
+              onMouseLeave={handleMouseLeaveDocumentos}
+            >
+              <NavDropdown.Item href="#action3">Admisión</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">Otra acción</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">Algo más aquí</NavDropdown.Item>
+            </NavDropdown>
+
+            <Nav.Link href="#">
+              CONTACTO
             </Nav.Link>
           </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
